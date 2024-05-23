4e0919c178ebffecca3178d6de97f551173eaad6
jorgegastonmartinez <jorge_gmartinez@hotmail.com>
2024-05-23 16:59:05 -0300

    listo products

--- segunda_pre_entrega/src/routes/products.router.js
+++ segunda_pre_entrega/src/routes/products.router.js
@@ -64,17 +64,105 @@ router.get("/products", async (req, res) => {
     }
 });
 
-router.post("/products", async (req, res) => {
+router.get("/products/:pid", async (req, res) => {
     try {
-        const product = new productModel(req.body);
-        await product.save();
-        res.json({ status: "success", payload: product });
+        let {pid} = req.params;
+
+        let product = await productModel.findById(pid);
+
+        if (!product) {
+            return res.status(400).send({error: "Producto no encontrado"})
+        }
+        res.send({result: "success", payload: product})
+
     } catch (error) {
-        console.error("Error al crear el producto:", error);
-        res
-          .status(500)
-          .json({ status: "error", message: "Error Interno del Servidor" });
+        console.error("Error al obtener el producto");
+        res.status(500).json({ error: "Error al obtener el producto" });
     }
 })
+
+router.post("/products", async (req, res) => {
+    let {title, description, code, price, stock, category } = req.body;
+
+    if(!title || !description || !code || !price || isNaN(stock) || !category){
+        console.log("Debes completar todos los campos");
+        return res.status(400).send({error: "Debes completar correctamente todos los campos"});
+    }
+
+    stock = Number(stock)
+    if (stock <= 0) {
+        console.log("El STOCK debe ser mayor que 0");
+        return res.status(400).send({error: "el campo STOCK debe ser mayor que 0"});
+    }
+
+    try {
+        const codeExists = await productModel.exists({ code: code });
+
+        if (codeExists) {
+            console.log("El campo code ya existe con ese número");
+            return res.status(400).send({error: "El campo code ya existe con ese número"})
+        }
+
+        let result = await productModel.create({
+          title,
+          description,
+          code,
+          price,
+          stock,
+          category,
+        });
+
+        res.send({result: "success", playload: result});
+
+    } catch (error) {
+        console.error("Error al crear el producto", error);
+        res.status(500).send({error: "Error al crear el producto"})
+    }
+})
+
+router.put("/products/:pid", async (req, res) => {
+    let {pid} = req.params;
+
+    let productToUpdate = req.body;
+
+    if (!productToUpdate.title || !productToUpdate.description || !productToUpdate.code || !productToUpdate.price || isNaN(productToUpdate.stock) || !productToUpdate.category) {
+        res.send({status: "Error", error: "Debe completar todos los campos del producto"})
+    }
+
+    try {
+        const existProduct = await productModel.findById(pid);
+        if (!existProduct) {
+            return res.status(400).send({error: "Producto no encontrado"})
+        }
+
+        const codeExists = await productModel.exists({
+          _id: { $ne: pid },
+          code: productToUpdate.code,
+        });
+
+        if (codeExists) {
+            return res.status(400).send({error: "El campo code ya está siendo utilizado por otro producto"})
+        }
+
+        let result = await productModel.updateOne({_id: pid}, productToUpdate);
+
+        res.send({result: "success", payload: result});
+
+    } catch (error) {
+        console.error("Error al actualizar el producto", error);
+        res.status(500).send({ error: "Error al actualizar el producto" });
+    }
+})
+
+router.delete("/products/:pid", async (req, res) => {
+    try {
+        let {pid} = req.params;
+    let result = await productModel.deleteOne({ _id: pid });
+    res.send({result: "success", payload: result});
+    } catch (error) {
+        console.error("El producto no se ha podido eliminar");
+        res.status(500).send({error: "El producto no se ha podido eliminar"})
+    }
+});
 
 export default router;
